e8a6cf81aa23ff824f058c629efbcbdb4b551dfc
selvakumarvin <68106063+selvakumarvin@users.noreply.github.com>
2022-12-02 10:39:01 +0530

    ToDo.js update

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -11,17 +11,19 @@ class ToDo extends React.Component {
             lastDay : '[Yesterday at] LT',
             sameDay : '[Today at] LT',
             nextDay : '[Tomorrow at] LT',
-            lastWeek : '[last] dddd [at] LT',
+            lastWeek : '[Last] dddd [at] LT',
             nextWeek : 'dddd [at] LT',
-            sameElse : 'L'
+            sameElse : 'DD-MMM-YYYY - HH:MM'
         };
+        const datetime = this.props.todo.date + 'T' + this.props.todo.time;
 
-        return (
-        <>
+        return (<>
             <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                     <div className="fw-bold">{this.props.todo.task}</div>
-                    <Moment parse="YYYY-MM-DD HH:mm"> {this.props.todo.date} {this.props.todo.time} </Moment> ( <Moment fromNow>{this.props.todo.date}</Moment> )
+                    <Moment calendar={calendarStrings}>
+                        {datetime} 
+                    </Moment> ( <Moment fromNow>{this.props.todo.date}</Moment> )
                 </div>
                 <span className="badge bg-primary rounded-pill">{this.props.todo.time}</span>
             </li>
